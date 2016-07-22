d7723af7047d82f1703dcb413c71b128f361d4cd
Jason Blickhan <jblickhan11@gmail.com>
2016-07-22 13:39:48 -0500

    floating title movement

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -19,7 +19,7 @@ var drops = [];
 //x below is the x coordinate
 //1 = y co-ordinate of the drop(same for every drop initially)
 for(var x = 0; x < columns; x++)
-	drops[x] = 1; 
+	drops[x] = c.height/font_size - 1; 
 
 //drawing the characters
 function draw()
@@ -29,7 +29,7 @@ function draw()
 	ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
 	ctx.fillRect(0, 0, c.width, c.height);
 	
-	ctx.fillStyle = "#0F0"; //green text
+	ctx.fillStyle = "#00f"; //green text
 	ctx.font = font_size + "px arial";
 	//looping over drops
 	for(var i = 0; i < drops.length; i++)
@@ -41,14 +41,44 @@ function draw()
 		
 		//sending the drop back to the top randomly after it has crossed the screen
 		//adding a randomness to the reset to make the drops scattered on the Y axis
-		if(drops[i]*font_size > c.height && Math.random() > 0.975)
-			drops[i] = 0;
-		
+		if(drops[i]*font_size < 0 && Math.random() > 0.975)
+			drops[i] = c.height / font_size;
+				
 		//incrementing Y coordinate
-		drops[i]++;
+		drops[i]--;
 	}
 }
+var pos = $('#home_container').position();
+var speed = 3;
+var xdir = speed, ydir = -speed;
+var buffer = 5;
+function move_title(){
+	pos = $('#home_container').position();
 
-setInterval(draw, 33);
+	if(pos.top <= buffer){
+		ydir = speed;
+	}
+	if(pos.top + $('#home_container').height() >= c.height - buffer){
+		ydir = -speed;
+	}
+	if(pos.left <= buffer){
+		xdir = speed;
+	}
+	if(pos.left + $('#home_container').width() >= c.width - buffer){
+		xdir = -speed;
+	}
+
+	$('#home_container').css({
+		'top':pos.top + ydir,
+		'left':pos.left + xdir
+	})
+
+}
+
+function loop(){
+	draw();
+	move_title();
+}
+setInterval(loop, 33);
 
 });
